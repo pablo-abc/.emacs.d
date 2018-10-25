fca005d025081271af7246810c9d7de7ab38f530
Pablo Berganza <pberganza@applaudostudios.com>
2018-10-25 17:50:19 -0600

    :sparkles: Flycheck check for local installation of eslint

--- init.el
+++ init.el
@@ -340,6 +340,20 @@
   )
 (use-package flycheck
   :ensure t
+  :config
+  ;; https://emacs.stackexchange.com/questions/21205/flycheck-with-file-relative-eslint-executable
+  (defun my/use-eslint-from-node-modules ()
+    "Get local eslint executable."
+    (let* ((root (locate-dominating-file
+                  (or (buffer-file-name) default-directory)
+                  "node_modules"))
+           (eslint (and root
+                        (expand-file-name "node_modules/eslint/bin/eslint.js"
+                                          root))))
+      (when (and eslint (file-executable-p eslint))
+        (setq-local flycheck-javascript-eslint-executable eslint))))
+
+  (add-hook 'flycheck-mode-hook #'my/use-eslint-from-node-modules)
   )
 (use-package restclient
   :ensure t
