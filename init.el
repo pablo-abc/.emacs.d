d56ffd8cabb8b6fe113c9e2569175a265bccf934
Pablo Berganza <pberganza@applaudostudios.com>
2018-12-11 12:21:02 -0600

    :sparkles: Add ranger, dockerfile mode and godoctor

--- init.el
+++ init.el
@@ -236,7 +236,7 @@
  '(org-confirm-babel-evaluate nil)
  '(package-selected-packages
    (quote
-    (company-go go-mode htmlize py-autopep8 julia-repl julia-mode lsp-ui company-lsp lsp-mode lsp-vue flycheck-flow rjsx-mode visual-regexp visual-regexp-steroids octave-mode gitmoji company-emoji emojify diminish google-this pipenv company-jedi elpy powerline dimmer focus unicode-fonts moe-theme ledger-mode atomic-chrome sql-indent ob-http ob-restclient plantuml-mode drawille xkcd beacon hacker-typer cargo flycheck-rust rust-mode clojure-mode cider zone-nyan ivy-hydra nyan-mode indium counsel swiper ivy markdown-mode editorconfig json-mode neotree vue-mode tide typescript-mode evil slack restclient company-tern ag xref-js2 js2-refactor exec-path-from-shell js-format magit apib-mode yaml-mode racket-mode ac-js2 use-package erc-hl-nicks weechat js2-mode smartparens auto-package-update web-mode php-mode flycheck)))
+    (ranger dockerfile-mode godoctor go-guru company-go go-mode htmlize py-autopep8 julia-repl julia-mode lsp-ui company-lsp lsp-mode lsp-vue flycheck-flow rjsx-mode visual-regexp visual-regexp-steroids octave-mode gitmoji company-emoji emojify diminish google-this pipenv company-jedi elpy powerline dimmer focus unicode-fonts moe-theme ledger-mode atomic-chrome sql-indent ob-http ob-restclient plantuml-mode drawille xkcd beacon hacker-typer cargo flycheck-rust rust-mode clojure-mode cider zone-nyan ivy-hydra nyan-mode indium counsel swiper ivy markdown-mode editorconfig json-mode neotree vue-mode tide typescript-mode evil slack restclient company-tern ag xref-js2 js2-refactor exec-path-from-shell js-format magit apib-mode yaml-mode racket-mode ac-js2 use-package erc-hl-nicks weechat js2-mode smartparens auto-package-update web-mode php-mode flycheck)))
  '(speedbar-show-unknown-files t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -331,6 +331,13 @@
 
 ;; Miscellaneous packages
 ;; ----------------------------------------------------------------------------------------------------
+(use-package dockerfile-mode
+  :ensure t
+  :defer t)
+
+(use-package ranger
+  :ensure t)
+
 (use-package google-this
   :ensure t
   :defer t
@@ -639,6 +646,14 @@
   :ensure t
   :config
   (add-hook 'before-save-hook 'gofmt-before-save))
+
+(use-package go-guru
+  :ensure t
+  :after (go-mode))
+
+(use-package godoctor
+  :ensure t
+  :after (go-mode))
 
 (use-package company-go
   :ensure t
