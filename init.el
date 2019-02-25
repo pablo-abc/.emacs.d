8add7aa74eda476160cb7c17150ba9c24d42ef32
Pablo Berganza <pberganza@applaudostudios.com>
2019-02-25 12:56:43 -0600

    :sparkles: Add midje, csharp and protobuf mode

--- init.el
+++ init.el
@@ -256,7 +256,7 @@
  '(org-confirm-babel-evaluate nil)
  '(package-selected-packages
    (quote
-    (flycheck-joker speed-type hy-mode lispy intero haskell-mode counsel-projectile projectile elisp-demos helpful docker carbon-now-sh lsp-ui which-key dockerfile-mode godoctor go-guru company-go go-mode htmlize py-autopep8 julia-repl julia-mode company-lsp flycheck-flow rjsx-mode visual-regexp visual-regexp-steroids octave-mode gitmoji company-emoji emojify diminish google-this pipenv company-jedi elpy powerline dimmer focus unicode-fonts moe-theme ledger-mode atomic-chrome sql-indent ob-http ob-restclient plantuml-mode drawille xkcd beacon hacker-typer cargo flycheck-rust rust-mode clojure-mode cider zone-nyan ivy-hydra nyan-mode indium counsel swiper ivy markdown-mode editorconfig json-mode neotree vue-mode tide typescript-mode evil restclient company-tern ag xref-js2 js2-refactor exec-path-from-shell js-format magit apib-mode yaml-mode racket-mode ac-js2 use-package erc-hl-nicks weechat js2-mode smartparens auto-package-update web-mode php-mode flycheck)))
+    (midje-mode csharp-mode protobuf-mode flycheck-joker speed-type hy-mode lispy intero haskell-mode counsel-projectile projectile elisp-demos helpful docker carbon-now-sh lsp-ui which-key dockerfile-mode godoctor go-guru company-go go-mode htmlize py-autopep8 julia-repl julia-mode company-lsp flycheck-flow rjsx-mode visual-regexp visual-regexp-steroids octave-mode gitmoji company-emoji emojify diminish google-this pipenv company-jedi elpy powerline dimmer focus unicode-fonts moe-theme ledger-mode atomic-chrome sql-indent ob-http ob-restclient plantuml-mode drawille xkcd beacon hacker-typer cargo flycheck-rust rust-mode clojure-mode cider zone-nyan ivy-hydra nyan-mode indium counsel swiper ivy markdown-mode editorconfig json-mode neotree vue-mode tide typescript-mode evil restclient company-tern ag xref-js2 js2-refactor exec-path-from-shell js-format magit apib-mode yaml-mode racket-mode ac-js2 use-package erc-hl-nicks weechat js2-mode smartparens auto-package-update web-mode php-mode flycheck)))
  '(speedbar-show-unknown-files t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -751,6 +751,9 @@
 (use-package clojure-mode
   :ensure t)
 
+(use-package midje-mode
+  :ensure t)
+
 (use-package cider
   :ensure t
   :config
@@ -956,6 +959,16 @@
 (use-package julia-repl
   :ensure t
   :hook (julia-mode . julia-repl-mode))
+
+;; C# configuration
+;; --------------------------------------------------------------------------------
+(use-package csharp-mode
+  :ensure t)
+
+;; Protocol Buffers configuration
+;; --------------------------------------------------------------------------------
+(use-package protobuf-mode
+  :ensure t)
 
 ;; Haskell configuration
 ;;---------------------------------------------------------------------------------
