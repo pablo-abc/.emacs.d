959fb4a0287c8f9b5ce51316b4cf858a55184d33
Pablo Berganza <pberganza@applaudostudios.com>
2019-07-02 12:16:13 -0600

    :sparkles: Add pomidor

--- init.el
+++ init.el
@@ -256,7 +256,7 @@
  '(org-confirm-babel-evaluate nil)
  '(package-selected-packages
    (quote
-    (csv-mode ess-smart-underscore ess poly-R polymode telephone-line dracula-theme emojify company graphql-mode ob-clojurescript langtool flycheck-vale csharp-mode protobuf-mode flycheck-joker speed-type hy-mode lispy intero haskell-mode counsel-projectile projectile elisp-demos helpful docker carbon-now-sh lsp-ui which-key dockerfile-mode godoctor go-guru company-go go-mode htmlize py-autopep8 julia-repl julia-mode company-lsp flycheck-flow rjsx-mode visual-regexp visual-regexp-steroids octave-mode gitmoji company-emoji diminish google-this pipenv company-jedi elpy powerline dimmer focus unicode-fonts moe-theme ledger-mode atomic-chrome sql-indent ob-http ob-restclient plantuml-mode drawille xkcd beacon hacker-typer cargo flycheck-rust rust-mode clojure-mode cider zone-nyan ivy-hydra nyan-mode indium counsel swiper ivy markdown-mode editorconfig json-mode neotree vue-mode tide typescript-mode evil restclient company-tern ag xref-js2 js2-refactor exec-path-from-shell js-format magit apib-mode yaml-mode racket-mode ac-js2 use-package erc-hl-nicks weechat js2-mode smartparens auto-package-update web-mode php-mode flycheck)))
+    (pomidor groovy-mode csv-mode ess-smart-underscore ess poly-R polymode telephone-line dracula-theme emojify company graphql-mode ob-clojurescript langtool flycheck-vale csharp-mode protobuf-mode flycheck-joker speed-type hy-mode lispy intero haskell-mode counsel-projectile projectile elisp-demos helpful docker carbon-now-sh lsp-ui which-key dockerfile-mode godoctor go-guru company-go go-mode htmlize py-autopep8 julia-repl julia-mode company-lsp flycheck-flow rjsx-mode visual-regexp visual-regexp-steroids octave-mode gitmoji company-emoji diminish google-this pipenv company-jedi elpy powerline dimmer focus unicode-fonts moe-theme ledger-mode atomic-chrome sql-indent ob-http ob-restclient plantuml-mode drawille xkcd beacon hacker-typer cargo flycheck-rust rust-mode clojure-mode cider zone-nyan ivy-hydra nyan-mode indium counsel swiper ivy markdown-mode editorconfig json-mode neotree vue-mode tide typescript-mode evil restclient company-tern ag xref-js2 js2-refactor exec-path-from-shell js-format magit apib-mode yaml-mode racket-mode ac-js2 use-package erc-hl-nicks weechat js2-mode smartparens auto-package-update web-mode php-mode flycheck)))
  '(speedbar-show-unknown-files t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -400,6 +400,20 @@
         (projectile-register-project-type 'pipenv '("Pipfile")
                                           :compile "pipenv install"))
     (error "Not defined: %s" "projectile-register-project-type")))
+
+(use-package pomidor
+  :ensure t
+  :config (setq pomidor-sound-tick nil
+                pomidor-sound-tack nil)
+  :hook (pomidor-mode . (lambda ()
+                          (display-line-numbers-mode -1) ; Emacs 26.1+
+                          (setq left-fringe-width 0 right-fringe-width 0)
+                          (setq left-margin-width 2 right-margin-width 0)
+                          ;; force fringe update
+                          (set-window-buffer nil (current-buffer)))))
+
+(use-package hacker-typer
+  :ensure t)
 
 (use-package counsel-projectile
   :ensure t
@@ -567,7 +581,7 @@
 
 (use-package apib-mode
   :ensure t
-  :mode "\\.apib\\'"
+  :mode ("\\.apib\\'" "\\.mson\\'")
   :bind (:map apib-mode-map
               ("C-c C-a P" . apiary-publish)
               ("C-c C-a p" . apiary-preview-current-buffer)
@@ -1064,6 +1078,12 @@
 ;; ------------------------------------------------------------------------------
 
 (use-package csv-mode
+  :ensure t)
+
+;; Groovy configuration
+;; ------------------------------------------------------------------------------
+
+(use-package groovy-mode
   :ensure t)
 
 ;; Org mode configuration
